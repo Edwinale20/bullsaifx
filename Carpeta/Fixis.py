e28362dfab49d4c1d580a2346259208ba610fb6a
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-04-26 12:49:19 -0600

    Update Fixis.py

--- Carpeta/Fixis.py
+++ Carpeta/Fixis.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import os
+import streamlit as st
+import plotly.express as px
+import numpy as np
+import plotly.graph_objects as go
+from io import BytesIO
+import requests 
+import plotly.io as pio
 
+st.set_page_config(page_title="Reporte de Coberturas Cigarros", page_icon="🚬", layout="wide", initial_sidebar_state="expanded")
+st.title("🚦 Reporte de Coberturas Cigarros🚬")
