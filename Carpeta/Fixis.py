b4590329c2be250515f600b97c81b8190af75fec
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-07-04 23:14:05 -0600

    Update Fixis.py

--- Carpeta/Fixis.py
+++ Carpeta/Fixis.py
@@ -10,7 +10,7 @@ import subprocess
 import requests
 from io import BytesIO
   
-
+ 
 st.set_page_config(page_title="Coberturas Cigarros y RRPS", page_icon="🚦", layout="wide", initial_sidebar_state="expanded")
 st.title("🚦 Coberturas Cigarros y RRPS 🚬")
 st.markdown("🆕 Se incluyen datos de la semana anterior.", unsafe_allow_html=True)
