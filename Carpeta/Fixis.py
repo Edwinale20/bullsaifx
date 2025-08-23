b472cce054bf5b438a76b540bd5a08a67214a6ec
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-08-23 12:25:02 -0600

    Update Fixis.py

--- Carpeta/Fixis.py
+++ Carpeta/Fixis.py
@@ -13,8 +13,8 @@ import io
 #----------------------------------------------------------------------------------------
 
    
-st.set_page_config(page_title="Reporte de Abasto y Clientes Comercial", page_icon="🏪", layout="wide", initial_sidebar_state="expanded")
-st.title("📊 Reporte de Abasto y Clientes comercial 🏪")
+st.set_page_config(page_title="Reporte de Abasto y Clientes", page_icon="🏪", layout="wide", initial_sidebar_state="expanded")
+st.title("📊 Reporte de Abasto y Clientes 🏪")
 st.markdown("✅ Arrastra aquí tu archivo de inventarios")
 st.markdown("✅ Esta app analiza en menos de 30 segundos las coberturas de todo el catálogo de tu categoría. Te ahorramos hasta 5 horas de trabajo semanales!")
 st.markdown("✅ Puedes identificar los productos en desabasto, y aquellos con oportunidades." "Además identificar pocas UPTD, con el fin de seguir el plan APT")
