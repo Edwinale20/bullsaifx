33f757b58bade53399488bd02d3d2da2583841e9
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-08-23 11:11:10 -0600

    Update Fixis.py

--- Carpeta/Fixis.py
+++ Carpeta/Fixis.py
@@ -18,7 +18,7 @@ st.title("📊 Reporte de Abasto y Clientes comercial 🏪")
 st.markdown("✅ Arrastra aquí tu archivo de inventarios")
 st.markdown("✅ Esta app analiza en menos de 30 segundos las coberturas de todo el catálogo de tu categoría. Te ahorramos hasta 5 horas de trabajo semanales!")
 st.markdown("✅ Puedes identificar los productos en desabasto, y aquellos con oportunidades." "Además identificar pocas UPTD, con el fin de seguir el plan APT")
-st.markdown("🔐 Esta app no guarda datos en la nube o en caché. Si deseas reiniciar la página solo da refresh a la página")
+st.markdown("🔐 Esta app no guarda datos en la nube o en caché. Si deseas reiniciar todo solo da refresh a la página")
 kpi_top = st.container()
 
 
